6d0932452e0f8fafed7627ac2e8691203091a700
hojune0330 <hojune0330@users.noreply.github.com>
2025-10-16 14:55:37 +0000

    feat(community): add right sidebar to app layout

--- src/community-app/src/app/AppLayout.tsx
+++ src/community-app/src/app/AppLayout.tsx
@@ -3,6 +3,7 @@ import Header from '../components/layout/Header'
 import AnnouncementBar from '../components/layout/AnnouncementBar'
 import BoardNav from '../components/layout/BoardNav'
 import LeftSidebar from '../components/layout/LeftSidebar'
+import RightSidebar from '../components/layout/RightSidebar'
 
 function AppLayout() {
   return (
@@ -11,13 +12,16 @@ function AppLayout() {
       <BoardNav />
       <AnnouncementBar />
       <main className="mx-auto w-full max-w-6xl px-4 py-6 sm:px-6 lg:px-8">
-        <div className="grid gap-6 lg:grid-cols-[260px,1fr]">
+        <div className="grid gap-6 lg:grid-cols-[260px,minmax(0,1fr)] xl:grid-cols-[260px,minmax(0,1fr),300px]">
           <aside className="hidden lg:block">
             <LeftSidebar />
           </aside>
           <section className="min-w-0 space-y-6">
             <Outlet />
           </section>
+          <aside className="hidden xl:block">
+            <RightSidebar />
+          </aside>
         </div>
       </main>
     </div>
